07a98d52fc6d9f0fb10d88e3ae27fff998be233d
Pedro Soto Soto <dex.pgsoto@gmail.com>
2024-10-28 22:37:10 +0000

    feat: add axios interceptor

--- src/common/util/index.ts
+++ src/common/util/index.ts
@@ -1,40 +1,19 @@
-import { JwtService } from '@nestjs/jwt';
-import { Response } from 'express';
+import axios from 'axios';
 
-import { IUserEntityDocument } from '../../modules/user/domain/entity';
-import { AppConfigService } from '../../config/app-config.service'; // Importar la clase de configuración
-
-export const generateAndRegisterTokens = async (
-  user: IUserEntityDocument,
-  remember: boolean,
-  res: Response,
-  appConfigService: AppConfigService
-) => {
-  // Crear instancia de JwtService con el secreto de la configuración
-  const jwtService = new JwtService({ secret: appConfigService.token.secret });
-
-  // Crear el payload con los datos necesarios
-  const payload = { userId: user._id, remember };
-
-  // Firmar el JWT
-  const token = jwtService.sign(payload);
-
-  // Registrar cookies con token y nivel de usuario
-  res.cookie('Authorization', `Bearer ${token}`, {
-    httpOnly: true,
-    secure: appConfigService.isProd,
-    maxAge: remember ? 30 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000, // Recordar durante 30 días si `remember` es true
+export function createAxiosInstance(url: string) {
+  const axiosInstance = axios.create({
+    baseURL: url,
+    timeout: 10000,
   });
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      // Puedes manejar el error de manera centralizada aquí
+      console.error('API Error:', error);
+      return Promise.reject(error);
+    }
+  );
+  return axiosInstance;
+}
 
-  // res.cookie(`${appConfig.token.nameLevel}`, user.level, {
-  //   httpOnly: true,
-  //   secure: appConfig.isProd,
-  //   maxAge: remember ? 30 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000,
-  // });
-};
-
-export const removeCookies = (res: Response, appConfig: AppConfigService) => {
-  // Eliminar cookies al cerrar sesión
-  res.clearCookie('Authorization');
-  // res.clearCookie(`${appConfig.token.nameLevel}`);
-};
+export default createAxiosInstance;
